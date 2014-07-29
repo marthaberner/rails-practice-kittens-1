38636ab2ea193c8f2db547e5889023b874667613
marthaberner <marthaberner.mb@gmail.com>
2014-07-29 14:18:53 -0600

    Refactor tests

--- spec/features/categorization_spec.rb
+++ spec/features/categorization_spec.rb
@@ -1,33 +1,15 @@
 require 'rails_helper'
 
 feature 'Categorizations' do
-  before do
+  scenario 'Categorization cannot be blank' do
     user = create_user email: "user@example.com"
     login(user)
     Kitten.create!(image: "http://i.imgur.com/tOzb0dUb.jpg")
-  end
-  scenario 'Categorization cannot be blank' do
-
     visit root_path
     find(".kitten-link").click
     click_on "Add Category"
 
     expect(page).to have_content("Category can't be blank")
-  end
-
-  scenario 'Categorization must be unique' do
-    Category.create!(name: "Cutest!")
-
-    visit root_path
-    find(".kitten-link").click
-    select "Cutest!", from: "categorization_category_id"
-    click_on "Add Category"
-
-    find(".kitten-link").click
-    select "Cutest!", from: "categorization_category_id"
-    click_on "Add Category"
-
-    expect(page).to have_content("Category has already been taken")
   end
 
   def login(user)
--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Categorization do
   it 'Validates the presence of a category' do
-    category = Category.create(name: 'Fuzzy')
-    kitten = Kitten.create(image: 'some_image')
+    category = Category.create!(name: 'Fuzzy')
+    kitten = Kitten.create!(image: 'some_image')
     categorization = Categorization.new(category_id: category.id, kitten_id: kitten.id )
 
     expect(categorization.valid?).to eq true
@@ -13,15 +13,15 @@ describe Categorization do
   end
 
   it 'Validates that a categorization is unique' do
-    category = Category.create(name: 'Fuzzy')
-    kitten = Kitten.create(image: 'some_image')
-    new_kitten = Kitten.create(image: 'some__other_image')
-    Categorization.create(category_id: category.id, kitten_id: kitten.id )
-    categorization_copy = Categorization.new(category_id: category.id, kitten_id: new_kitten.id )
+    category = Category.create!(name: 'Fuzzy')
+    kitten = Kitten.create!(image: 'some_image')
+    Categorization.create!(category: category, kitten_id: kitten.id )
+    categorization = Categorization.new(category: category)
 
-    expect(categorization_copy.valid?).to eq true
+    expect(categorization.valid?).to eq true
 
-    categorization_copy.kitten_id = kitten.id
-    expect(categorization_copy.valid?).to eq false
+    categorization.kitten_id = kitten.id
+    expect(categorization.valid?).to eq false
+    expect(categorization.errors[:category]).to include("has already been taken")
   end
 end
